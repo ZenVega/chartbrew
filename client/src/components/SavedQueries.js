107b7ef83ad600cf0627246780d8abfb3849653e
ZenVega <50147356+ZenVega@users.noreply.github.com>
2020-10-14 17:50:05 +0200

    end of session

--- client/src/components/SavedQueries.js
+++ client/src/components/SavedQueries.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import {
@@ -11,40 +11,36 @@ import { secondaryTransparent } from "../config/colors";
 /*
   Contains the project creation functionality
 */
-class SavedQueries extends Component {
-  constructor(props) {
-    super(props);
+function SavedQueries(props) {
 
-    this.state = {
-      loading: false,
-      error: "",
-    };
-  }
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
 
-  componentDidMount() {
-    this._getSavedQueries();
-  }
+  useEffect(() => {
+    _getSavedQueries();
+  });
 
-  _getSavedQueries = () => {
-    const { getSavedQueries } = this.props;
+  const _getSavedQueries = () => {
+    const { getSavedQueries } = props;
 
-    this.setState({ loading: true });
-    const { project, type } = this.props;
+    setLoading(true);
+    const { project, type } = props;
     getSavedQueries(project.id, type)
       .then(() => {
-        this.setState({ loading: false });
+        setLoading(false);
       })
       .catch(() => {
-        this.setState({ error: true, loading: false });
+        setLoading(false);
+        setError(true);
       });
-  }
+  };
 
-  _onEditQueryConfirmation = (query) => {
+  const _onEditQueryConfirmation = (query) => {
     this.setState({ editQuery: query });
-  }
+  };
 
-  _onEditQuery = () => {
-    const { updateSavedQuery, project } = this.props;
+  const _onEditQuery = () => {
+    const { updateSavedQuery, project } = props;
     const { savedQuerySummary, editQuery } = this.state;
 
     this.setState({ editLoading: true });
@@ -65,14 +61,14 @@ class SavedQueries extends Component {
           savedQuerySummary: "",
         });
       });
-  }
+  };
 
-  _onRemoveQueryConfirmation = (queryId) => {
+  const _onRemoveQueryConfirmation = (queryId) => {
     this.setState({ removeQuery: queryId });
-  }
+  };
 
-  _onRemoveQuery = () => {
-    const { deleteSavedQuery, project } = this.props;
+  const _onRemoveQuery = () => {
+    const { deleteSavedQuery, project } = props;
     const { removeQuery } = this.state;
     this.setState({ removeLoading: true });
     deleteSavedQuery(project.id, removeQuery)
@@ -88,165 +84,156 @@ class SavedQueries extends Component {
           removeLoading: false,
         });
       });
-  }
+  };
 
-  render() {
-    const {
-      loading, error, editQuery, editLoading, savedQuerySummary, removeQuery,
-      removeLoading,
-    } = this.state;
-    const {
-      savedQueries, onSelectQuery, selectedQuery, style
-    } = this.props;
-    return (
-      <div style={{ ...styles.container, ...style }}>
-        <Loader active={loading} />
+  return (
+    <div style={{ ...styles.container, ...style }}>
+      <Loader active={loading} />
 
-        {error
-          && (
-          <Message negative>
-            <Message.Header>Could not get your saved queries</Message.Header>
-            <p>Try to refresh the page or get in touch with us to fix the issue.</p>
-          </Message>
-          )}
+      {error
+        && (
+        <Message negative>
+          <Message.Header>Could not get your saved queries</Message.Header>
+          <p>Try to refresh the page or get in touch with us to fix the issue.</p>
+        </Message>
+        )}
 
-        {savedQueries.length > 0
-          && (
-          <List
-            divided
-            selection
-            verticalAlign="middle"
+      {savedQueries.length > 0
+        && (
+        <List
+          divided
+          selection
+          verticalAlign="middle"
+        >
+          {savedQueries.map((query) => {
+            return (
+              <List.Item
+                key={query.id}
+                style={selectedQuery === query.id ? styles.selectedItem : {}}
+              >
+                <List.Content floated="right">
+                  <Popup
+                    trigger={(
+                      <Button
+                        icon
+                        positive
+                        onClick={() => onSelectQuery(query)}
+                      >
+                        <Icon name="check" />
+                      </Button>
+                    )}
+                    content="Use this query"
+                    position="top left"
+                  />
+
+                  <Popup
+                    trigger={(
+                      <Button
+                        icon
+                        secondary
+                        loading={editQuery && editLoading && editQuery.id === query.id}
+                        onClick={() => _onEditQueryConfirmation(query)}
+                      >
+                        <Icon name="pencil" />
+                      </Button>
+                    )}
+                    content="Edit the summary"
+                    position="top center"
+                  />
+
+                  <Popup
+                    trigger={(
+                      <Button
+                        icon
+                        negative
+                        onClick={() => _onRemoveQueryConfirmation(query.id)}
+                      >
+                        <Icon name="x" />
+                      </Button>
+                    )}
+                    content="Remove the saved query"
+                    position="top right"
+                  />
+                </List.Content>
+
+                <List.Content verticalAlign="middle">
+                  <List.Header>{query.summary}</List.Header>
+                  <List.Description>{`created by ${query.User.name}`}</List.Description>
+                </List.Content>
+              </List.Item>
+            );
+          })}
+        </List>
+        )}
+      {savedQueries.length < 1 && !loading
+        && <p><i>{"The project doesn't have any saved queries yet"}</i></p>}
+
+      {/* Update query modal */}
+      <Modal open={!!editQuery} size="small" onClose={() => this.setState({ editQuery: null })}>
+        <Header
+          content="Edit the query"
+          inverted
+        />
+        <Modal.Content>
+          <Header size="small">Edit the description of the query</Header>
+          <Input
+            placeholder="Type a summary here"
+            value={savedQuerySummary ? savedQuerySummary /* eslint-disable-line */
+              : editQuery ? editQuery.summary : ""}
+            fluid
+            onChange={(e, data) => this.setState({ savedQuerySummary: data.value })}
+          />
+        </Modal.Content>
+        <Modal.Actions>
+          <Button
+            onClick={() => this.setState({ editQuery: null })}
           >
-            {savedQueries.map((query) => {
-              return (
-                <List.Item
-                  key={query.id}
-                  style={selectedQuery === query.id ? styles.selectedItem : {}}
-                >
-                  <List.Content floated="right">
-                    <Popup
-                      trigger={(
-                        <Button
-                          icon
-                          positive
-                          onClick={() => onSelectQuery(query)}
-                        >
-                          <Icon name="check" />
-                        </Button>
-                      )}
-                      content="Use this query"
-                      position="top left"
-                    />
+            Close
+          </Button>
+          <Button
+            primary
+            disabled={!savedQuerySummary}
+            icon
+            labelPosition="right"
+            loading={editLoading}
+            onClick={_onEditQuery}
+          >
+            <Icon name="checkmark" />
+            Save the query
+          </Button>
+        </Modal.Actions>
+      </Modal>
 
-                    <Popup
-                      trigger={(
-                        <Button
-                          icon
-                          secondary
-                          loading={editQuery && editLoading && editQuery.id === query.id}
-                          onClick={() => this._onEditQueryConfirmation(query)}
-                        >
-                          <Icon name="pencil" />
-                        </Button>
-                      )}
-                      content="Edit the summary"
-                      position="top center"
-                    />
-
-                    <Popup
-                      trigger={(
-                        <Button
-                          icon
-                          negative
-                          onClick={() => this._onRemoveQueryConfirmation(query.id)}
-                        >
-                          <Icon name="x" />
-                        </Button>
-                      )}
-                      content="Remove the saved query"
-                      position="top right"
-                    />
-                  </List.Content>
-
-                  <List.Content verticalAlign="middle">
-                    <List.Header>{query.summary}</List.Header>
-                    <List.Description>{`created by ${query.User.name}`}</List.Description>
-                  </List.Content>
-                </List.Item>
-              );
-            })}
-          </List>
-          )}
-        {savedQueries.length < 1 && !loading
-          && <p><i>{"The project doesn't have any saved queries yet"}</i></p>}
-
-        {/* Update query modal */}
-        <Modal open={!!editQuery} size="small" onClose={() => this.setState({ editQuery: null })}>
-          <Header
-            content="Edit the query"
-            inverted
-          />
-          <Modal.Content>
-            <Header size="small">Edit the description of the query</Header>
-            <Input
-              placeholder="Type a summary here"
-              value={savedQuerySummary ? savedQuerySummary /* eslint-disable-line */
-                : editQuery ? editQuery.summary : ""}
-              fluid
-              onChange={(e, data) => this.setState({ savedQuerySummary: data.value })}
-            />
-          </Modal.Content>
-          <Modal.Actions>
-            <Button
-              onClick={() => this.setState({ editQuery: null })}
-            >
-              Close
-            </Button>
-            <Button
-              primary
-              disabled={!savedQuerySummary}
-              icon
-              labelPosition="right"
-              loading={editLoading}
-              onClick={this._onEditQuery}
-            >
-              <Icon name="checkmark" />
-              Save the query
-            </Button>
-          </Modal.Actions>
-        </Modal>
-
-        {/* Update query modal */}
-        <Modal open={!!removeQuery} size="small" basic onClose={() => this.setState({ removeQuery: null })}>
-          <Header
-            icon="exclamation triangle"
-            content="Are you sure you want to remove the query?"
-            inverted
-          />
-          <Modal.Content>
-            <p>{"This action will be permanent."}</p>
-          </Modal.Content>
-          <Modal.Actions>
-            <Button
-              onClick={() => this.setState({ removeQuery: null })}
-            >
-              Close
-            </Button>
-            <Button
-              negative
-              icon
-              labelPosition="right"
-              loading={removeLoading}
-              onClick={this._onRemoveQuery}
-            >
-              <Icon name="x" />
-              Remove the query
-            </Button>
-          </Modal.Actions>
-        </Modal>
-      </div>
-    );
-  }
+      {/* Update query modal */}
+      <Modal open={!!removeQuery} size="small" basic onClose={() => this.setState({ removeQuery: null })}>
+        <Header
+          icon="exclamation triangle"
+          content="Are you sure you want to remove the query?"
+          inverted
+        />
+        <Modal.Content>
+          <p>{"This action will be permanent."}</p>
+        </Modal.Content>
+        <Modal.Actions>
+          <Button
+            onClick={() => this.setState({ removeQuery: null })}
+          >
+            Close
+          </Button>
+          <Button
+            negative
+            icon
+            labelPosition="right"
+            loading={removeLoading}
+            onClick={_onRemoveQuery}
+          >
+            <Icon name="x" />
+            Remove the query
+          </Button>
+        </Modal.Actions>
+      </Modal>
+    </div>
+  );
 }
 
 const styles = {
